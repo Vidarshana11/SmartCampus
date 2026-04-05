0bdc79265c5568d341303ade1aa9ec32ed171e81
Vidarshana11 <vidarshanajayasena92@gmail.com>
2026-04-06 00:16:41 +0530

    refactor: remove unused imports in ProfilePictureController

--- backend/src/main/java/com/smartcampus/api/user/ProfilePictureController.java
+++ backend/src/main/java/com/smartcampus/api/user/ProfilePictureController.java
@@ -7,12 +7,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
